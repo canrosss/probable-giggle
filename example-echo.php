979efdbd9c0a669def9277978681a1aecdcb281c
Canros <canros.mx@gmail.com>
2018-02-23 01:52:34 -0600

    Actualizando..

---
+++ example-echo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+<?php
+  echo "<h2> Php is Fun!</h2>"
+  echo "<h2> Hello world</h2>"
+
+?>
+</body>
+</html>
