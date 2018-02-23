192a08cecc6e1a9f1bd18745bdc209da0dfc23da
Canros <canros.mx@gmail.com>
2018-02-23 01:47:50 -0600

    Actualizando..

--- hello-world.php
+++ hello-world.php
@@ -4,7 +4,7 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-$name = "Carlos";
+$name = "Carlos 2";
 echo "<h1>$name</h1>"
 ?>
 
